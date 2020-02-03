0a759687e9a7f7a6c1f239f491fa7de41791fe28
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-02-03 21:53:04 +0100

    second middleware

--- src/index.js
+++ src/index.js
@@ -17,9 +17,19 @@ app.get('/', (req, res) => {
 });
 
 app.use((req, res, next) => {
-  const error = new Error(`Not found - ${req.originalUrl}`;
+  const error = new Error(`Not found - ${req.originalUrl}`);
   res.status(404);
   next(error);
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🏀' : error.stack,
+  });
 });
 
 const port = process.env.PORT || 1337;
