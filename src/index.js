39e982c1fc7304138aff96dbffab3d80bf828435
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-02-03 22:03:01 +0100

    middlewares in separate files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const middlewares = require('./middlewares');
 
 const app = express();
 app.use(morgan('common'));
@@ -16,21 +17,8 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use((req, res, next) => {
-  const error = new Error(`Not found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-});
-
-// eslint-disable-next-line no-unused-vars
-app.use((error, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? '🏀' : error.stack,
-  });
-});
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler;
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
