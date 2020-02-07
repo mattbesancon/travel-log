9973a73c0dd7bf7d47b875869fbbb763038f1012
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-02-07 16:29:44 +0100

    api logs

--- src/index.js
+++ src/index.js
@@ -4,28 +4,32 @@ const helmet = require('helmet');
 const cors = require('cors');
 const middlewares = require('./middlewares');
 const mongoose = require('mongoose');
+const logs = require('./api/logs');
 
 require('dotenv').config();
+
+
+
+const app = express();
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
 });
 
-const app = express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
 }));
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+app.use('/api/logs', logs);
 
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello World',
   });
 });
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler;
 
 const port = process.env.PORT;
 app.listen(port, () => {
