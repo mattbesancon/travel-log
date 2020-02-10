89d07c6ad597b903b58f76c20c613251cdb30cf2
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-02-10 22:40:00 +0100

    mapbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-
 import React, { useState } from 'react';
 import ReactMapGL from 'react-map-gl';
 
-function Map() {
+const App = () => {
   const [viewport, setViewport] = useState({
     width: 400,
     height: 400,
@@ -40,7 +13,10 @@ function Map() {
   return (
     <ReactMapGL
       {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={setViewport}
     />
   );
 }
+
+export default App;
