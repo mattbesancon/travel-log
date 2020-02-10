141a0718d26ca400cca59c33a880794c3c7d6f2e
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-02-10 16:57:23 +0100

    react app

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+
+function Map() {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={setViewport}
+    />
+  );
+}
