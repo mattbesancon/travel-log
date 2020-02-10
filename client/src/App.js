d0304b90f83c6e960e694af755fd516596140879
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-02-10 22:45:19 +0100

    full screen

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import ReactMapGL from 'react-map-gl';
 
 const App = () => {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
+    width: '100vw',
+    height: '100vh',
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 8
