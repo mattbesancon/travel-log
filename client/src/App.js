ecf1e5decd89aefb3177d7a2da620d22613c53ac
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-02-11 21:09:13 +0100

    last changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
 
 import { listLogEntries } from './API';
 
 const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -15,7 +16,7 @@ const App = () => {
   useEffect(() => {
     (async () => {
       const logEntries = await listLogEntries();
-      console.log(logEntries);
+      setLogEntries(logEntries);
     })();
   }, []);
 
@@ -24,7 +25,21 @@ const App = () => {
       {...viewport}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={setViewport}
-    />
+    >
+      {
+        logEntries.map(entry => (
+          <Marker
+            key={entry._id}
+            latitude={entry.latitude}
+            longitude={entry.longitude}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <div>{entry.title}</div>
+          </Marker>
+        ))
+      }
+     </ReactMapGL>
   );
 }
 
